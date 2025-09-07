d9e57c962455a3ee7a3bab1934272dfd80f2e392
David15Barrera <david1315barrera@gmail.com>
2025-09-07 13:41:52 -0600

    start Proyect

---
+++ financeService/src/main/java/com/service/financeService/FinanceServiceApplication.java
@@ -0,0 +1,19 @@
+package com.service.financeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+
+@EnableFeignClients
+@ConfigurationPropertiesScan
+@PropertySource("file:${user.dir}/.env")
+@SpringBootApplication
+public class FinanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceServiceApplication.class, args);
+	}
+
+}
---
+++ financeService/src/main/java/com/service/financeService/infrastructure/property/RsaProperty.java
@@ -0,0 +1,12 @@
+package com.service.financeService.infrastructure.property;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.security.interfaces.RSAPublicKey;
+
+
+@ConfigurationProperties("security.rsa")
+public record RsaProperty(
+        RSAPublicKey publicKey) {
+}
